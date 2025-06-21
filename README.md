3db04887c442960b98351cab7ad635f500133959
wyctorfogos <wyctor.fogos@gmail.com>
2025-06-21 20:59:10 -0300

    feat: First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Create enviroment:
+```bash
+    conda create -n eda-sentences
+```
+
+# To activate this environment, use
+```bash
+    conda activate eda-sentences
+```
+
+# To deactivate an active environment, use
+```bash
+    conda deactivate
+```
---
+++ src/scripts/utils/load_dataset.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+
+def load_data(file_folder_path:str):
+    try:
+        data = pd.read_csv(filepath_or_buffer=file_folder_path, sep=",")
+        return data
+    except FileNotFoundError:
+        print("Arquivo não encontrado!")
+    
